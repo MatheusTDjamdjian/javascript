46638e15f915141ff6f935ec56248a329c146948
Matheus Tasso Djamdjian <matheustassodjamdjian@gmail.com>
2024-04-24 22:08:12 -0300

    Finalizando ex001

--- cursojs/modulo-d/exercícios/ex001/script.js
+++ cursojs/modulo-d/exercícios/ex001/script.js
@@ -1,15 +1,22 @@
-function carregar() {
-    var msg = window.document.getElementById(`msg`)
-    var img = window.document.getElementById(`imagem`)
+function Carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
-        img.scr = 'manhã.png'
-    } else if (hora >= 12 && hora < 18) {
-        img.scr = 'tarde.png'
+        // Bom Dia!
+        img.src = 'manha.png'
+        document.body.style.background = '#fee46a'
+    } else if (hora >= 12 && hora <= 18) {
+        // Boa Tarde!
+        img.src = 'tarde.png'
+        document.body.style.background = '#ff9618'
     } else {
-        img.scr = 'noite.png'
-    }
+        // Boa Noite!
+        img.src = 'noite.png'
+        document.body.style.background = '#216ac5'
+
+    }  
 }
 
