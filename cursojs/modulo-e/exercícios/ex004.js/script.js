a22db186f5bda9c261c17f007b7bcfe2f8324844
Matheus <matheustassodjamdjian@gmail.com>
2024-05-06 13:59:31 -0300

    fx ex004.js

--- cursojs/modulo-e/exercícios/ex004.js/script.js
+++ cursojs/modulo-e/exercícios/ex004.js/script.js
@@ -6,8 +6,13 @@ function tabuada() {
     } else {
         var n = Number(num.value)
         var c = 1
+        tab.innerHTML = ''
         while (c <= 10) {
             var item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
         }
     }
 }
